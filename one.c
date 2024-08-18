cff69ed710ca3fbdf7d9ace5587d55bd12351620
sanbai <2845556715@qq.com>
2024-08-18 15:48:57 +0800

    C_code/main

---
+++ one.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int a == 1;
+    printf(a);
+    return 1;
+}
---
+++ text.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf(a);
+    return 0;
+}
+
+void a()
+{
+    return 33;
+}
